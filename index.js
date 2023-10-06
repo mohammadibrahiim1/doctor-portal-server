90e1a0fafe41bfbfabc6c45964c1c5405d40c004
mohammadibrahiim1 <mohammadibrahiim1@gmail.com>
2023-10-06 12:36:59 +0600

    get doctors by category

--- index.js
+++ index.js
@@ -1,28 +1,27 @@
-
 // const cookieSession = require('cookie-session');
-const passport = require('passport');
-const passportSetup = require('./passport')
+const passport = require("passport");
+const passportSetup = require("./passport");
 
 // import { Strategy as GoogleStrategy } from 'passport-google-oauth';
 // import { Strategy as FacebookStrategy } from 'passport-facebook';
 // import expressSession from express-session
 
-
-
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const authRoute = require('./routes/auth')
+const authRoute = require("./routes/auth");
 const port = process.env.PORT || 5000;
-
-
 
 // Enable CORS
 app.use(cors());
-app.use(require('express-session')({ secret: 'keyboard cat', resave: true, saveUninitialized: true }));
-
-
+app.use(
+  require("express-session")({
+    secret: "keyboard cat",
+    resave: true,
+    saveUninitialized: true,
+  })
+);
 
 // Connect to MongoDB
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
@@ -43,6 +42,7 @@ async function run() {
   const servicesCollection = db.collection("services");
   const doctorsCollection = db.collection("doctors");
   const articlesCollection = db.collection("articles");
+  const usersCollection = db.collection("users");
   try {
     app.get(`/api/v1/appointments`, async (req, res) => {
       const cursor = appointmentsCollection.find({});
@@ -52,9 +52,28 @@ async function run() {
 
     // get all doctors
     app.get(`/api/v1/doctors`, async (req, res) => {
-      const cursor = doctorsCollection.find({});
-      const result = await cursor.toArray();
-      res.send({ status: true, items: result.length, data: result });
+      if (req.query.category) {
+        const category = req.query.category;
+        const result = await doctorsCollection.find({ category }).toArray();
+        res.send({
+          status: true,
+          items: result.length,
+          data: result,
+        });
+      } else {
+        const query = {};
+        const result = await doctorsCollection.find(query).toArray();
+        res.send({ status: true, items: result.length, data: result });
+      }
+      // const query = {};
+      // const category = req.query.category;
+      // if (category) {
+      //   query.category = category;
+      // }
+      // const cursor = doctorsCollection.find(query).toArray();
+      // res.send(cursor);
+      // // const result = await cursor.toArray();
+      // // res.send({ status: true, items: result.length, data: result });
     });
 
     // get  doctor by id
@@ -64,6 +83,16 @@ async function run() {
       const result = await doctorsCollection.findOne(query);
       res.send({ status: true, data: result });
     });
+
+    // get doctor by category
+
+    // app.get(`/api/v1/doctors`, async (req, res) => {
+    //   const category = req.query.category;
+    //   const query = {};
+    //   if (category) {
+    //     query.category = category
+    //   }
+    // });
 
     // get services data
     app.get(`/api/v1/services`, async (req, res) => {
@@ -86,12 +115,34 @@ async function run() {
       const result = await cursor.toArray();
       res.send({ status: true, items: result.length, data: result });
     });
+
     // get articles data by id
     app.get(`/api/v1/article/:id`, async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
       const result = await articlesCollection.findOne(query);
       res.send({ status: true, data: result });
+    });
+
+    // users collection
+
+    app.post("/api/v1/user", async (req, res) => {
+      const user = req.body;
+      const result = await usersCollection.insertOne(user);
+      console.log(result);
+      res.send(result);
+    });
+
+    app.get("/api/v1/user/:email", async (req, res) => {
+      const email = req.params.email;
+
+      const result = await usersCollection.findOne({ email });
+
+      if (result?.email) {
+        return res.send({ status: true, data: result });
+      }
+
+      res.send({ status: false });
     });
 
     // authentication
@@ -103,24 +154,23 @@ async function run() {
     app.use(passport.initialize());
     app.use(passport.session());
 
-    app.use(cors({
-      origin: 'http://localhost:3000',
-      methods: 'GET,POST,PUT,DELETE',
-      credentials: true
-    }))
+    app.use(
+      cors({
+        origin: "http://localhost:3000",
+        methods: "GET,POST,PUT,DELETE",
+        credentials: true,
+      })
+    );
 
+    app.use("/auth", authRoute);
 
-    app.use('/auth', authRoute)
-
-
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
   }
 }
 run().catch((error) => console.log(error));
-
-
-
 
 app.get("/", (req, res) => {
   res.send(`hello doctor-portal`);
